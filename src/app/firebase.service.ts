ff7c354d8aa086f7aae933583eb55d4e710b942d
nlko <nospam1@thomasson.fr>
2019-02-22 22:07:32 +0100

    Add note support

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,5 +1,30 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore} from '@angular/fire/firestore';
+import { AngularFirestore, QueryDocumentSnapshot} from '@angular/fire/firestore';
+
+export class Note {
+  id: string = null;
+  body = '';
+  tags?: string[] = [];
+
+  constructor({id, body, tags} = {id: null, body: '', tags: [], }) {
+    this.id = id || null;
+    this.body = body || '';
+    this.tags = tags || [];
+  }
+
+  get title() {
+    const getFirstLine = (text) => {
+      const line = this.body.split('\n').find(l => l.trim().length > 0);
+      return line;
+    };
+
+    return  (this.body && getFirstLine(this.body)) || 'Untitled';
+  }
+
+  static fromRawData(data: QueryDocumentSnapshot<{}> ) {
+    return new Note({id: data.id, body: '', tags: [], ...data.data()});
+  }
+}
 
 @Injectable({
   providedIn: 'root'

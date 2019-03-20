5942c27eba8e23362c0f03bf1f960cda193e2ac0
nlko <nospam1@thomasson.fr>
2019-03-20 10:41:25 +0000

    Add delete confirmation

--- src/app/components/ui/note-delete-button/note-delete-button.component.ts
+++ src/app/components/ui/note-delete-button/note-delete-button.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-note-delete-button',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note-delete-button.component.scss']
 })
 export class NoteDeleteButtonComponent implements OnInit {
+  @Output() delete = new EventEmitter<void>();
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+
+  onDelete() {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+        width: '250px',
+        data: {}
+      });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed with ' + result);
+      if (result) {
+        this.delete.emit();
+      }
+    });
+
   }
 
 }
--- src/app/components/ui/ui.module.ts
+++ src/app/components/ui/ui.module.ts
@@ -3,7 +3,8 @@ import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { IconComponent } from './icon/icon.component';
 import { MatIconModule, MatCardModule, MatListModule, MatButtonModule, MatSlideToggleModule,
-  MatTreeModule, MatFormFieldModule, MatInputModule, MatCheckboxModule, MatChipsModule, MatAutocompleteModule, } from '@angular/material';
+  MatTreeModule, MatFormFieldModule, MatInputModule, MatCheckboxModule, MatChipsModule,
+  MatAutocompleteModule, MatDialogModule, } from '@angular/material';
 import { NoteTitleCardComponent } from './note-title-card/note-title-card.component';
 import { ListNotesComponent } from './list-notes/list-notes.component';
 import { NoteCreateButtonComponent } from './note-create-button/note-create-button.component';
@@ -12,11 +13,14 @@ import { NoteViewEditToggleComponent } from './note-view-edit-toggle/note-view-e
 import { TreeTagsComponent } from './tree-tags/tree-tags.component';
 import { NoteSearchInputComponent } from './note-search-input/note-search-input.component';
 import { SearchInputComponent } from './search-input/search-input.component';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 @NgModule({
   declarations: [IconComponent, NoteTitleCardComponent, ListNotesComponent, NoteCreateButtonComponent,
-    NoteDeleteButtonComponent, NoteViewEditToggleComponent, TreeTagsComponent, NoteSearchInputComponent, SearchInputComponent ],
+    NoteDeleteButtonComponent, NoteViewEditToggleComponent, TreeTagsComponent,
+    NoteSearchInputComponent, SearchInputComponent, ConfirmDialogComponent ],
   exports: [IconComponent, NoteTitleCardComponent, ListNotesComponent, NoteCreateButtonComponent,
-    NoteDeleteButtonComponent, NoteViewEditToggleComponent, TreeTagsComponent, NoteSearchInputComponent , SearchInputComponent],
+    NoteDeleteButtonComponent, NoteViewEditToggleComponent, TreeTagsComponent,
+    NoteSearchInputComponent , SearchInputComponent, ConfirmDialogComponent],
   imports: [
     CommonModule,
     FormsModule,
@@ -31,7 +35,11 @@ import { SearchInputComponent } from './search-input/search-input.component';
     MatCheckboxModule,
     MatChipsModule,
     MatAutocompleteModule,
+    MatDialogModule,
     ReactiveFormsModule,
   ],
+  entryComponents: [
+    ConfirmDialogComponent
+  ]
 })
 export class UiModule { }
